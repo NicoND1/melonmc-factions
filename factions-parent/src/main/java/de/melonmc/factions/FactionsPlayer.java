931a97e7ee9515c502a5ad16aeab3bfe491d1c24
Nico Didszuweit <nicondlouis@gmail.com>
2019-01-20 16:17:15 +0100

    Add spigot dependency and add factions player

---
+++ factions-parent/src/main/java/de/melonmc/factions/FactionsPlayer.java
@@ -0,0 +1,18 @@
+package de.melonmc.factions;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.UUID;
+
+/**
+ * @author Nico_ND1
+ */
+@AllArgsConstructor
+@Data
+public class FactionsPlayer {
+
+    private final UUID uuid;
+    private final String name;
+    private final Player
+
+}
