cae99e8df11dbc8bc8159e24b068e4bf48ee7564
Nico Didszuweit <nicondlouis@gmail.com>
2019-01-20 19:20:50 +0100

    Delete HomeManager

--- factions-parent/src/main/java/de/melonmc/factions/Factions.java
+++ factions-parent/src/main/java/de/melonmc/factions/Factions.java
@@ -1,13 +1,10 @@
 package de.melonmc.factions;
 import de.melonmc.factions.database.DatabaseSaver;
-import de.melonmc.factions.home.HomeManager;
 
 /**
  * @author Nico_ND1
  */
 public interface Factions {
-
-    HomeManager getHomeManager();
 
     DatabaseSaver getDatabaseSaver();
 
