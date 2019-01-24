c519c309c87b0d67b0f21bd68a442efb8d4cf4b2
Nico Didszuweit <nicondlouis@gmail.com>
2019-01-24 21:37:25 +0100

    Finish sethome command

--- factions-bukkit/src/main/java/de/melonmc/bukkit/command/home/SetHomeCommand.java
+++ factions-bukkit/src/main/java/de/melonmc/bukkit/command/home/SetHomeCommand.java
@@ -1,8 +1,13 @@
 package de.melonmc.bukkit.command.home;
 import de.melonmc.factions.Factions;
+import de.melonmc.factions.Messages;
 import de.melonmc.factions.command.ICommand;
+import de.melonmc.factions.home.Home;
 import de.melonmc.factions.player.FactionsPlayer;
+import de.melonmc.factions.util.ConfigurableLocation;
 import org.bukkit.entity.Player;
+
+import java.util.Optional;
 
 /**
  * @author Nico_ND1
@@ -20,14 +25,32 @@ public class SetHomeCommand implements ICommand<Player> {
 
     @Override
     public Result onExecute(Player player, String label, String[] args) {
+        if (args.length != 1) return Result.WRONG_ARGUMENTS;
+        final String homeName = args[0];
+
         Factions.getInstance().getDatabaseSaver().findHomes(new FactionsPlayer(player), homes -> {
             final int maxHomes = FactionsPlayer.HOMES_PER_PLAYER;
 
-            if(homes.size() >= maxHomes) {
-
+            if (homes.size() >= maxHomes) {
+                player.sendMessage(Messages.TOO_MANY_HOMES.getMessage(FactionsPlayer.HOMES_PER_PLAYER));
                 return;
             }
+
+            final Optional<Home> optionalHome = homes.stream()
+                .filter(home -> home.getName().equalsIgnoreCase(homeName))
+                .findAny();
+
+            if (optionalHome.isPresent()) {
+                final Home home = optionalHome.get();
+                home.setLocation(new ConfigurableLocation(player.getLocation()));
+
+                Factions.getInstance().getDatabaseSaver().saveHome(home, () -> player.sendMessage(Messages.HOME_UPDATED.getMessage(home.getName())));
+            } else {
+                final Home home = new Home(new FactionsPlayer(player), homeName, new ConfigurableLocation(player.getLocation()));
+
+                Factions.getInstance().getDatabaseSaver().saveHome(home, () -> player.sendMessage(Messages.HOME_SET.getMessage(home.getName())));
+            }
         });
-        return null;
+        return Result.SUCCESSFUL;
     }
 }
